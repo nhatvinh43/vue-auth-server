3a0256e8bc758b33e3e72e18444b401d1269398d
Phan Nhật Vinh <phannhatvinh10a@gmail.com>
2021-02-19 11:32:43 +0700

    Remove unused passport jwt strategy

--- utils/passport.js
+++ utils/passport.js
@@ -5,7 +5,7 @@ const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
 const jwtOptions = {};
 const jwt = require('jsonwebtoken');
-const { addAccount, login, getUser } = require('../services/AccountServices');
+const { addAccount, login } = require('../services/AccountServices');
 
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = 'hichic';
@@ -15,15 +15,19 @@ passport.use('signup', new localStrategy({
     usernameField: 'email',
     passwordField: 'password'
 },
-    async (email, password, done) => {
-        try {
+    async (email, password, done) =>
+    {
+        try
+        {
             const account = await addAccount({ email, password });
-            if (!account) {
+            if (!account)
+            {
                 return done(null, {}, "Error creating account");
             }
             const token = jwt.sign({ _id: account._id, email: account.email }, jwtOptions.secretOrKey);
             return done(null, token);
-        } catch (e) {
+        } catch (e)
+        {
             console.log(e);
             return done(e);
         }
@@ -32,25 +36,20 @@ passport.use('signup', new localStrategy({
 passport.use('login', new localStrategy({
     usernameField: 'email',
     passwordField: 'password'
-}, async (email, password, done) => {
-    try {
+}, async (email, password, done) =>
+{
+    try
+    {
         const account = await login({ email, password });
-        if (!account) {
+        if (!account)
+        {
             return done(null, {}, "Invalid credentials");
         }
         const result = jwt.sign({ _id: account._id, email: account.email }, jwtOptions.secretOrKey)
         return done(null, result);
-    } catch (e) {
+    } catch (e)
+    {
         console.log(e);
         return done(e);
     }
 }))
-
-passport.use('jwt', new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-    const account = getUser({ _id: jwt_payload._id });
-    if (user) {
-        next(null, account);
-    } else {
-        next(null, false);
-    }
-}));
